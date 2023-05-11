f5849506bf0034395a04e88b6d7b0b165c96e3ad
Lily Yu <lilyyu2926@gmail.com>
2023-05-11 14:55:38 -0500

    Flask_test

--- flask_program.py
+++ flask_program.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+print("Loading configuration....")
+# Here, you can add code to actually load a configuration if you need to.
+print("Done loading configuration")
 
+if __name__ == '__main__':
+    app.run()
