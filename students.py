6f2b4d3871d84ff8393867fa1147ec3b2c3fd4b3
sdoyle9 <129123293+sdoyle9@users.noreply.github.com>
2023-05-18 09:06:31 -0500

    deleted changes made before

--- students.py
+++ students.py
@@ -1,18 +1,4 @@
-# import os
-from flask import Flask, render_template, request
-# from flask_sqlalchemy import SQLAlchemy
-
-# from sqlalchemy.sql import func
-
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] =\
-#         'sqlite:///' + os.path.join(basedir, 'database.db')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.template_folder = 'templates'
