d670a94c61e0d1c2b4ad4967fd31c48066e370d2
Your Name <you@example.com>
2023-05-18 09:37:50 -0500

    db stuff

--- students.py
+++ students.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-db = SQLAlchemy(app)
 
 db_user = 'root'
 db_pass = 'sprintern2023'
 db_name = 'FieldApp'
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:sprintern2023@localhost/FieldApp"
-
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:sprintern2023@localhost/FieldApp"
+app.config['SECRET_KEY']= "secret key"
+db =SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    
+
 def __repr__(self):
         return '<User %r>' % self.username
+    
+    
 app.template_folder = 'templates'
 
 
@@ -35,7 +36,7 @@ def map():
 def form():
     return render_template('form.html')
 
-# @app.route("/map")
+# @app.route("/maep")
 # def map():
 #     return render_template('map.html')
 
@@ -43,6 +44,7 @@ if __name__ == '__main__':
     print("Loading configuration....")
     # Here, you can add code to actually load a configuration if you need to.
     print("Done loading configuration")
+    db.create_all()
     app.run(debug = False)
     # app.run(debug=True)
 
