7dbca4e08b9c8d740b303d19e1f4eb3971c21fdd
Your Name <you@example.com>
2023-05-22 15:41:06 -0500

    database

--- students.py
+++ students.py
@@ -59,9 +59,9 @@ class User(db.Model):
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
 # app.config['SECRET_KEY']= "secret key"
 # db =SQLAlchemy(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
+# app.config['SECRET_KEY']= "secret key"
+# db =SQLAlchemy(app)
 
 # class User(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -98,38 +98,38 @@ class Project(db.Model):
 
 
 #  *------------------------------------------------------------------*   Lily code
-# python students.py
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from views import home, login, map_page, savepage
+# # python students.py
+# from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+# from views import home, login, map_page, savepage
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
+# app.config['SECRET_KEY']= "secret key"
+# db =SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    salary = db.Column(db.Float, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     age = db.Column(db.Integer, nullable=False)
+#     address = db.Column(db.String(120), nullable=False)
+#     salary = db.Column(db.Float, nullable=False)
 
 
-class Test(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+# class Test(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50), unique=True, nullable=False)
 
-app.add_url_rule('/', 'home', home)
-app.add_url_rule('/login', 'login', login)
-app.add_url_rule('/map', 'map_page', map_page)
-app.add_url_rule('/savepage', 'savepage', savepage)
+# app.add_url_rule('/', 'home', home)
+# app.add_url_rule('/login', 'login', login)
+# app.add_url_rule('/map', 'map_page', map_page)
+# app.add_url_rule('/savepage', 'savepage', savepage)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()  # Drop all existing tables
+        # db.drop_all()  # Drop all existing tables
         db.create_all()  # Create new tables according to the current models
     app.run(debug=False)
 #  *------------------------------------------------------------------*  END Lily code
