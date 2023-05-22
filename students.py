d2fc466dc5a8bb30a63a0205c8a133a89f34d0df
Lily Yu <lilyyu2926@gmail.com>
2023-05-22 14:45:52 -0500

    test

--- students.py
+++ students.py
@@ -42,19 +42,6 @@ from views import home, login, map_page, savepage
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    salary = db.Column(db.Float, nullable=False)
-
-=======
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
 app.config['SECRET_KEY']= "secret key"
 db =SQLAlchemy(app)
@@ -71,7 +58,6 @@ def map():
 @app.route('/form')
 def form():
     return render_template('form.html')
->>>>>>> 672ed60c7d18494e2a7612cc1d3880d0aa772356
 
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -82,12 +68,40 @@ app.add_url_rule('/login', 'login', login)
 app.add_url_rule('/map', 'map_page', map_page)
 app.add_url_rule('/savepage', 'savepage', savepage)
 
-<<<<<<< HEAD
+
+#  *------------------------------------------------------------------*   Lily code
+# python students.py
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from views import home, login, map_page, savepage
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
+app.config['SECRET_KEY']= "secret key"
+db =SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    salary = db.Column(db.Float, nullable=False)
+
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+app.add_url_rule('/', 'home', home)
+app.add_url_rule('/login', 'login', login)
+app.add_url_rule('/map', 'map_page', map_page)
+app.add_url_rule('/savepage', 'savepage', savepage)
+
 if __name__ == '__main__':
     with app.app_context():
         db.drop_all()  # Drop all existing tables
         db.create_all()  # Create new tables according to the current models
     app.run(debug=False)
-=======
-# python students.py
->>>>>>> 672ed60c7d18494e2a7612cc1d3880d0aa772356
+#  *------------------------------------------------------------------*  END Lily code
