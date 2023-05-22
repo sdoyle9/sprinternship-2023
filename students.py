7fcadf77e8d4c0dfdccf6c67012a3f172d11c7e5
Lily Yu <lilyyu2926@gmail.com>
2023-05-22 14:50:05 -0500

    test

--- students.py
+++ students.py
@@ -42,20 +42,6 @@ from views import home, login, map_page, savepage
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-=======
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    salary = db.Column(db.Float, nullable=False)
-
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
 app.config['SECRET_KEY']= "secret key"
 db =SQLAlchemy(app)
@@ -82,7 +68,6 @@ app.add_url_rule('/login', 'login', login)
 app.add_url_rule('/map', 'map_page', map_page)
 app.add_url_rule('/savepage', 'savepage', savepage)
 
-<<<<<<< HEAD
 
 #  *------------------------------------------------------------------*   Lily code
 # python students.py
@@ -114,15 +99,9 @@ app.add_url_rule('/login', 'login', login)
 app.add_url_rule('/map', 'map_page', map_page)
 app.add_url_rule('/savepage', 'savepage', savepage)
 
-=======
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
 if __name__ == '__main__':
     with app.app_context():
         db.drop_all()  # Drop all existing tables
         db.create_all()  # Create new tables according to the current models
     app.run(debug=False)
-<<<<<<< HEAD
 #  *------------------------------------------------------------------*  END Lily code
-=======
-# python students.py
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
--- views.py
+++ views.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-from flask import render_template
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -11,7 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('home_page.html')
 
-<<<<<<< HEAD
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Thundermandy12!@127.0.0.1/fieldapp"
 app.config['SECRET_KEY']= "secret key"
 db =SQLAlchemy(app)
@@ -28,8 +23,6 @@ from flask import render_template
 def home():
     return render_template('home_page.html')
 
-=======
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
 def login():
     return render_template('login.html')
 
@@ -38,14 +31,4 @@ def map_page():
 
 def savepage():
     return render_template('savepage.html')
-<<<<<<< HEAD
 #  *------------------------------------------------------------------*   END Lily code
-=======
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Thundermandy12!@127.0.0.1/fieldapp"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
-
-@app.route('/')
-def main_index():
-    return "Blueprint hello!"
->>>>>>> c2efd8942429852d9048cab43d8f4d51fb04023e
