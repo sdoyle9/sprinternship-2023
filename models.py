f6a311251799853f7d680b4efd1ca10a9494dd34
sdoyle9 <129123293+sdoyle9@users.noreply.github.com>
2023-05-22 12:53:38 -0500

    change spacing

--- models.py
+++ models.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from .extensions import db 
+from views import db 
 from flask_sqlalchemy import SQLAlchemy
 
 
 
 class MyModel(db.Model):
     project = db.Column(db.String(150), nullable = False, unique = True)
-    
     
     def __repr__(self):
         return '<Name %r>' % self.name
