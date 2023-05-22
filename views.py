c37ac327498b85b5d1c35646e3fa941bd50b6047
sdoyle9 <129123293+sdoyle9@users.noreply.github.com>
2023-05-22 12:36:54 -0500

    updated views.py

--- views.py
+++ views.py
@@ -1,7 +1,15 @@
 from flask import Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Thundermandy12!@127.0.0.1/fieldapp"
+app.config['SECRET_KEY']= "secret key"
+db =SQLAlchemy(app)
+
+@app.route('/')
 def main_index():
     return "Blueprint hello!"
