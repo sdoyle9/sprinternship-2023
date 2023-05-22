73a3fb34ac930bbf4a9ed7b441a5b232e8e2232d
Lily Yu <lilyyu2926@gmail.com>
2023-05-22 16:17:35 -0500

    lily submit

--- views.py
+++ views.py
@@ -1,23 +1,21 @@
-from flask import Blueprint
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+# from flask import Blueprint
+# from flask_sqlalchemy import SQLAlchemy
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-def home():
-    return render_template('home_page.html')
+# def home():
+#     return render_template('home_page.html')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Thundermandy12!@127.0.0.1/fieldapp"
-app.config['SECRET_KEY']= "secret key"
-db =SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Thundermandy12!@127.0.0.1/fieldapp"
+# app.config['SECRET_KEY']= "secret key"
+# db =SQLAlchemy(app)
 
-@app.route('/')
-<<<<<<< HEAD
-def login():
-    return render_template('login.html')
-=======
-def main_index():
-    return "Blueprint hello!"
+# @app.route('/')
+# def login():
+#     return render_template('login.html')
+# def main_index():
+#     return "Blueprint hello!"
 
 
 #  *------------------------------------------------------------------*   Lily 
@@ -37,7 +35,3 @@ def savepage():
     return render_template('savepage.html')
 <<<<<<< HEAD
 #  *------------------------------------------------------------------*   END Lily 
-=======
-#  *------------------------------------------------------------------*   END Lily code
->>>>>>> 7dbca4e08b9c8d740b303d19e1f4eb3971c21fdd
->>>>>>> f810a849c764c3b90ae276ac7f1ebe103fe48fc8
