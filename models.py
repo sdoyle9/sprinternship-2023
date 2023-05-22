672ed60c7d18494e2a7612cc1d3880d0aa772356
sdoyle9 <129123293+sdoyle9@users.noreply.github.com>
2023-05-22 13:02:26 -0500

    change name of sample table I created

--- models.py
+++ models.py
@@ -3,8 +3,7 @@ from views import db
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-class MyModel(db.Model):
+class Projects(db.Model):
     project = db.Column(db.String(150), nullable = False, unique = True)
     
     def __repr__(self):
