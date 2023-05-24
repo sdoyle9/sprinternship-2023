9b5e3e1978c0d01ec1c9fefb174f405426ec78bd
Your Name <you@example.com>
2023-05-24 10:53:23 -0500

    database

--- students.py
+++ students.py
@@ -49,12 +49,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/raabiahazeez/appField.
 app.config['SECRET_KEY']= "secret key"
 db =SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    salary = db.Column(db.Float, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     age = db.Column(db.Integer, nullable=False)
+#     address = db.Column(db.String(120), nullable=False)
+#     salary = db.Column(db.Float, nullable=False)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/meleenatorres/FieldApp.db"
 # app.config['SECRET_KEY']= "secret key"
@@ -69,12 +69,6 @@ class User(db.Model):
 #     email = db.Column(db.String(120), unique=True, nullable=False)
 
 @app.route("/")
-def map():
-    # you can change xxx.html
-    return render_template('login.html')
-@app.route('/form')
-def form():
-    return render_template('form.html')
 
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -95,26 +89,6 @@ class Project(db.Model):
     longitude = db.Column(db.Float)
     notes = db.Column(db.Text)
 
-class Perimeter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    lat1= db.Column(db.Integer, nullable=False)
-    long1= db.Column(db.Integer, nullable=False)
-    lat2= db.Column(db.Integer, nullable=False)
-    long2= db.Column(db.Integer, nullable=False)
-    lat3= db.Column(db.Integer, nullable=False)
-    long3= db.Column(db.Integer, nullable=False)
-    lat4= db.Column(db.Integer, nullable=True)
-    long4= db.Column(db.Integer, nullable=True)
-    lat5= db.Column(db.Integer, nullable=True)
-    long5= db.Column(db.Integer, nullable=True)
-    lat6= db.Column(db.Integer, nullable=True)
-    long6= db.Column(db.Integer, nullable=True)
-   
-
-   
-
-   
-
 
 #  *------------------------------------------------------------------*   Lily code
 # # python students.py
@@ -129,26 +103,40 @@ class Perimeter(db.Model):
 # app.config['SECRET_KEY']= "secret key"
 # db =SQLAlchemy(app)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     age = db.Column(db.Integer, nullable=False)
-#     address = db.Column(db.String(120), nullable=False)
-#     salary = db.Column(db.Float, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    salary = db.Column(db.Float, nullable=False)
 
+class Perimeter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lat1= db.Column(db.Integer, nullable=False)
+    long1= db.Column(db.Integer, nullable=False)
+    lat2= db.Column(db.Integer, nullable=False)
+    long2= db.Column(db.Integer, nullable=False)
+    lat3= db.Column(db.Integer, nullable=False)
+    long3= db.Column(db.Integer, nullable=False)
+    lat4= db.Column(db.Integer, nullable=True)
+    long4= db.Column(db.Integer, nullable=True)
+    lat5= db.Column(db.Integer, nullable=True)
+    long5= db.Column(db.Integer, nullable=True)
+    lat6= db.Column(db.Integer, nullable=True)
+    long6= db.Column(db.Integer, nullable=True)
 
 # class Test(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     name = db.Column(db.String(50), unique=True, nullable=False)
 
-# app.add_url_rule('/', 'home', home)
-# app.add_url_rule('/login', 'login', login)
-# app.add_url_rule('/map', 'map_page', map_page)
-# app.add_url_rule('/savepage', 'savepage', savepage)
+app.add_url_rule('/', 'home', home)
+app.add_url_rule('/login', 'login', login)
+app.add_url_rule('/map', 'map_page', map_page)
+app.add_url_rule('/savepage', 'savepage', savepage)
 
 if __name__ == '__main__':
     with app.app_context():
         # db.drop_all()  # Drop all existing tables
         db.create_all()  # Create new tables according to the current models
     app.run(debug=False)
-#  *------------------------------------------------------------------*  END Lily code
+#  *------------------------------------------------------------------*  END Lily 
