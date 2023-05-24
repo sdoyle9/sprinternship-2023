b40e431a04111a10449dcefb0c1f20674ca8174b
Your Name <you@example.com>
2023-05-24 10:35:47 -0500

    database stuff

--- students.py
+++ students.py
@@ -28,7 +28,7 @@
 #     print("Loading configuration....")
 #     # Here, you can add code to actually load a configuration if you need to.
 #     print("Done loading configuration")
-#     with app.app_context():
+#     with app.app_conInteger():
 #         db.create_all()
 #     app.run(debug = False)
 #     # app.run(debug=True)
@@ -45,7 +45,7 @@ app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/yufangyang/fieldapp.db"
 # app.config['SECRET_KEY']= "secret key"
  
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////raabiahazeez/sprinternship-2023-3/fieldapp.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/raabiahazeez/appField.db"
 app.config['SECRET_KEY']= "secret key"
 db =SQLAlchemy(app)
 
@@ -88,13 +88,32 @@ app.add_url_rule('/savepage', 'savepage', savepage)
 #project data 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    number = db.Column(db.String(20))
-    category = db.Column(db.String(50))
+    name = db.Column(db.String(50),nullable=False)
+    number = db.Column(db.Integer,nullable =False)
+    category = db.Column(db.String(50),nullable = False)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-    perimeter_points = db.Column(db.String(200))
     notes = db.Column(db.Text)
+
+class Perimeter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lat1= db.Column(db.Integer, nullable=False)
+    long1= db.Column(db.Integer, nullable=False)
+    lat2= db.Column(db.Integer, nullable=False)
+    long2= db.Column(db.Integer, nullable=False)
+    lat3= db.Column(db.Integer, nullable=False)
+    long3= db.Column(db.Integer, nullable=False)
+    lat4= db.Column(db.Integer, nullable=True)
+    long4= db.Column(db.Integer, nullable=True)
+    lat5= db.Column(db.Integer, nullable=True)
+    long5= db.Column(db.Integer, nullable=True)
+    lat6= db.Column(db.Integer, nullable=True)
+    long6= db.Column(db.Integer, nullable=True)
+   
+
+   
+
+   
 
 
 #  *------------------------------------------------------------------*   Lily code
